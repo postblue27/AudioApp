c818a67327af773b1e7d03b6768b79722edec784
Nazar <nazar121099@gmail.com>
2020-01-13 16:07:12 -0800

    AudioController updated

--- audioapp.API/Controllers/AudioController.cs
+++ audioapp.API/Controllers/AudioController.cs
@@ -1,7 +1,11 @@
 using audioapp.API.Data;
+using audioapp.API.Helpers;
 using audioapp.API.Models;
+using AutoMapper;
+using CloudinaryDotNet;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -14,13 +18,30 @@ namespace audioapp.API.Controllers
     public class AudioController : ControllerBase
     {
         private readonly DataContext _context;
-        public AudioController(DataContext conetxt){
+        private readonly IMapper _mapper;
+        private readonly IOptions<CloudinarySettings> _cloudinaryConfig;
+        private Cloudinary _cloudinary;
+
+        public AudioController(DataContext conetxt, IMapper mapper,
+        IOptions<CloudinarySettings> cloudinaryConfig)
+        {
+            _cloudinaryConfig = cloudinaryConfig;
+            _mapper = mapper;
             _context = conetxt;
+
+            Account acc = new Account(
+                _cloudinaryConfig.Value.CloudName,
+                _cloudinaryConfig.Value.ApiKey,
+                _cloudinaryConfig.Value.ApiSecret
+            );
+
+            _cloudinary = new Cloudinary(acc);
         }
-        [HttpGet]
-        public async Task<IActionResult> GetAudio(){
-            var tracks = await _context.Tracks.ToListAsync();
-            return Ok(tracks);
-        }
+    [HttpGet]
+    public async Task<IActionResult> GetAudio()
+    {
+        var tracks = await _context.Tracks.ToListAsync();
+        return Ok(tracks);
     }
+}
 }
