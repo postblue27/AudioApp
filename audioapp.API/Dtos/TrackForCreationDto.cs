ae874b259fda3090ddd52f1ebd122f2521754331
Nazar <nazar121099@gmail.com>
2020-01-15 01:40:10 -0800

    Added TrackName and PerformerName to upload

--- audioapp.API/Dtos/TrackForCreationDto.cs
+++ audioapp.API/Dtos/TrackForCreationDto.cs
@@ -4,6 +4,8 @@ namespace audioapp.API.Dtos
 {
     public class TrackForCreationDto
     {
+        public string TrackName { get; set; }
+        public string PerformerName { get; set; }
         public string Url { get; set; }
         public IFormFile File { get; set; }
         public string PublicId { get; set; }
--- audioapp.API/Dtos/TrackForReturnDto.cs
+++ audioapp.API/Dtos/TrackForReturnDto.cs
@@ -3,6 +3,8 @@ namespace audioapp.API.Dtos
     public class TrackForReturnDto
     {
         public int TrackId { get; set; }
+        public string TrackName { get; set; }
+        public string PerformerName { get; set; }
         public string Url { get; set; }
         public string PublicId { get; set; }
     }
