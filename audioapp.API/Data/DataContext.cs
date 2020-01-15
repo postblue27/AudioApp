2e220927fa4bbac35ddc894e95e761791a36aeef
Nazar <nazar121099@gmail.com>
2020-01-15 14:52:41 -0800

    User table and migration added

--- audioapp.API/Data/DataContext.cs
+++ audioapp.API/Data/DataContext.cs
@@ -7,5 +7,6 @@ namespace audioapp.API.Data
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options){ }
         public DbSet<Track> Tracks { get; set; }        
+        public DbSet<User> Users { get; set; }
     }
 }
