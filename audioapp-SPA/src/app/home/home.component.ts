df8972d71d64f5f6ff8af8e3315bea8b59341f86
Nazar <nazar121099@gmail.com>
2020-01-21 19:29:41 -0800

    track table design implemented

--- audioapp-SPA/src/app/home/home.component.ts
+++ audioapp-SPA/src/app/home/home.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, SimpleChanges } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
 import { faPause } from '@fortawesome/free-solid-svg-icons';
+import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons';
+import { inherits } from 'util';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +13,10 @@ import { faPause } from '@fortawesome/free-solid-svg-icons';
 export class HomeComponent implements OnInit {
   registerMode = false;
   tracks: any;
+  faPlay = faPlay;
+  faPause = faPause;
+  faArrowAltCircleDown = faArrowCircleDown;
+  activeTrackColor = 'rgb(70, 0, 100)';
   @Output() activeTrack = new EventEmitter();
 
   constructor(private http: HttpClient) { }
@@ -29,6 +35,44 @@ export class HomeComponent implements OnInit {
     }, error => {
       console.log(error);
     });
+  }
+
+  addListeners() {
+    const trackDivs = document.getElementsByClassName('trackDiv');
+    console.log(trackDivs.length);
+    for (let i = 0; i < trackDivs.length; i++) {
+      const current = trackDivs[i] as HTMLElement;
+      current.addEventListener('click', () => {
+        current.style.backgroundColor = 'grey';
+        console.log(current);
+      });
+    }
+  }
+
+  trackDivClick(id: string) {
+    const trackDiv = document.getElementById(id);
+    const trackDivs = document.getElementsByClassName('trackDiv');
+    for (let i = 0; i < trackDivs.length; i++) {
+      const current = trackDivs[i] as HTMLElement;
+      current.style.backgroundColor = 'inherit';
+      current.style.borderBottom = '1px solid rgb(45,45,45)';
+    }
+    trackDiv.style.backgroundColor = this.activeTrackColor;
+    trackDiv.style.borderBottom = '1px solid rgb(180, 0, 255)';
+  }
+
+  trackDivMouseOver(id: string) {
+    const trackDiv = document.getElementById(id);
+    if (trackDiv.style.backgroundColor !== this.activeTrackColor) {
+      trackDiv.style.backgroundColor = 'rgb(40,40,40)';
+    }
+  }
+
+  trackDivMouseOut(id: string) {
+    const trackDiv = document.getElementById(id);
+    if(trackDiv.style.backgroundColor !== this.activeTrackColor) {
+      trackDiv.style.backgroundColor = 'inherit';
+    }
   }
 
   cancelRegisterMode(registerMode: boolean) {
